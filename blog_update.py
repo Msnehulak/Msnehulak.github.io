e9cd04387a15cfa8cc17eae7474949cb339d8aaa
Msnehulak <mxmhrmn@gmail.com>
2026-05-16 16:52:46 +0200

    fix link

--- blog_update.py
+++ blog_update.py
@@ -18,9 +18,9 @@ class UpdateBlog:
     <nav>
         <div class="logo">SnehulakTV_</div>
         <div class="odkazy">
-            <a href="/index.html">Home</a>
-            <a href="/projects.html">Projects</a>
-            <a href="/blogs.html">blogs</a>
+            <a href="https://msnehulak.github.io/Snehulak.tv/">Home</a>
+            <a href="https://msnehulak.github.io/Snehulak.tv/blogs.html">Projects</a>
+            <a href="https://msnehulak.github.io/Snehulak.tv/blogs.html">blogs</a>
         </div>
     </nav>
 
